0dc17d89c84bb5f8b5dc7a19e948be3a2772c7de
Alexey Denisov <bauworkml@gmail.com>
2024-03-29 12:36:38 +0300

    Написал создание карточек из массива и удаление

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+const cardsList = document.querySelector('.places__list');
+
+function addCard(cardName, cardImgLink) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = cardName;
+  cardElement.querySelector('.card__image').src = cardImgLink;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', function(event) {
+    const eventTarget = event.target;
+    const parentCard = eventTarget.closest('.card');
+    parentCard.remove();
+  });
+  cardsList.append(cardElement);
+}
+
+initialCards.forEach( function(item) {
+  addCard(item.name, item.link);
+});
+
